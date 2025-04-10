f26df3ed5a80f9caf7d1c653eb8dcfb7d90a51ea
Drakkhonian <felipe.a.p.vieira@hotmail.com>
2025-04-09 21:53:40 -0300

    module changes

---
+++ manaforge/structures.js
@@ -0,0 +1,304 @@
+export const attack_shape_map = [[10, 11, 10, 10, 6, 12, 13, 12],
+[11, 11, 10, 7, 6, 7, 13, 13],
+[10, 10, 4, 4, 7, 12, 6, 12],
+[10, 4, 10, 6, 4, 6, 8, 12],
+[0, 4, 5, 5, 4, 9, 6, 12],
+[0, 4, 0, 8, 4, 2, 8, 8],
+[1, 1, 4, 4, 9, 8, 3, 3],
+[0, 1, 0, 0, 2, 2, 3, 2]];
+
+export const metal_list = ["Menos Bronze",
+"Valsena Iron",
+"Granz Steel",
+"Laurent Silver",
+"Wendel Silver",
+"Beiser Gold",
+"Ishe Platinum",
+"Lorimar Iron",
+"Altena Alloy",
+"Maia Lead",
+"Orichalcum"
+];
+
+export const wood_list = ["Oak Wood",
+"Holly Wood",
+"Baobab Wood",
+"Ebony Wood",
+"Maple Wood",
+"Dior Wood",
+"Ash Wood",
+"Fossil Wood"
+];
+
+export const stone_list = ["Marble",
+"Obsidian",
+"Pedan Stone",
+"Gaia's Tears"
+];
+
+export const hide_list = ["Animal Hide",
+"Gator Skin",
+"Centaur Hide",
+"Dragon Skin"
+];
+
+export const scales_list = ["Fish Scales",
+"Lizard Scales",
+"Snake Scales",
+"Dragon Scales"
+];
+
+export const bone_list = ["Animal Bone",
+"Ivory",
+"Cursed Bone",
+"Fossil"
+];
+
+export const fabric_list = ["Topple Cotton",
+"Sultan's Silk",
+"Judd Hemp",
+"Altena Felt"
+];
+
+export const rock_list = ["Jacobini Rock",
+"Halley Rock",
+"Ankh Rock",
+"Vinek Rock",
+"Tuttle Rock",
+"Nemesis Rock",
+"Biella Rock",
+"Swifte Rock"
+];
+
+export const misc_list = ["Adamantite",
+"Fullmetal",
+"Coral",
+"Tortoiseshell",
+"Seashell",
+"Emerald",
+"Pearl",
+"Lapis Lazuli"
+];
+
+export const coin_list = ["Wisp Gold",
+"Shade Gold",
+"Dryad Gold",
+"Aura Gold",
+"Salamander Gold",
+"Gnome Gold",
+"Jinn Gold",
+"Undine Gold",
+"Wisp Silver",
+"Shade Silver",
+"Dryad Silver",
+"Aura Silver",
+"Salamander Silver",
+"Gnome Silver",
+"Jinn Silver",
+"Undine Silver"
+];
+
+export const mana_stone_list = ["Firestone",
+"Earthstone",
+"Windstone",
+"Waterstone"
+];
+
+export const mana_crystal_list = ["Sun Crystal",
+"Moon Crystal",
+"Glow Crystal",
+"Chaos Crystal"
+];
+
+export const seed_list = ["Round Seed",
+"Oblong Seed",
+"Crooked Seed",
+"Big Seed",
+"Small Seed",
+"Long Seed",
+"Flat Seed",
+"Spiny Seed"
+];
+
+export const produce_list = ["Bellgrapes",
+"Diceberry",
+"Mangolephant",
+"Loquat-Shoe",
+"Pear o' Heels",
+"Squalphin",
+"Citrisquid",
+"Springanana",
+"Peach Puppy",
+"Apricat",
+"Applesock",
+"Whalamato",
+"Pine o'Clock",
+"Fishy Fruit",
+"Boarmelon",
+"Rhinoloupe",
+"Orcaplant",
+"Garlicrown",
+"Honey Onion",
+"Sweet Moai",
+"Spiny Carrot",
+"Conchurnip",
+"Cornflower",
+"Cabbadillo",
+"Needlettuce",
+"Cherry Bombs",
+"Masked Potato",
+"Lilipods",
+"Rocket Papaya",
+"Orange'opus",
+"Bumpkin",
+"Heart Mint",
+"Spade Basil",
+"Dialaurel",
+"Gold Clover",
+"Mush-in-a-Box",
+"Toadstoolshed"
+];
+
+export const meat_list = ["Animal Meat",
+"Bug Meat",
+"Lizard Meat",
+"Fish Meat",
+"Bird Meat",
+"Morph Meat",
+"Demon Meat",
+"Dragon Steak",
+"Odd Meat",
+"Magical Meat",
+"Rotton Meat"
+];
+
+export const fang_list =["Sharp Claw",
+"Poison Fang",
+"Giant's Horn",
+"Pincer",
+"Healing Claw",
+"Zombie Claw",
+"Vampire Fang"
+];
+
+export const eye_list =["Little Eye",
+"Sleepy Eye",
+"Silly Eye",
+"Dangerous Eye",
+"Angry Eye",
+"Blank Eye",
+"Creepy Eye",
+"Wicked Eye"
+];
+
+export const feather_list = ["Angel Feather",
+"Raven Feather",
+"Clear Feather",
+"Moth Wing",
+"Flaming Quill",
+"White Feather"
+];
+
+export const bottle_list = ["Aroma Oil",
+"Dragon Blood",
+"Acid",
+"Holy Water",
+"Ether"
+];
+
+export const vial_list = ["Mercury",
+"Stinky Breath",
+"Ghost's Howl",
+"Dragon's Breath",
+"Damsel's Sigh"
+];
+
+export const urn_list =["Electricity",
+"Moss",
+"Ear of Wheat"
+];
+
+export const pellet_list = ["Baked Roach",
+"Blackened Bat"
+];
+
+export const powder_list =["Sulfur",
+"Poison Powder",
+"Sleepy Powder",
+"Knockout Dust",
+"Rust",
+"Grave Dirt",
+"Ash"
+];
+
+export const pouch_list = ["Hairball",
+"Needle",
+"Mirror Piece",
+"Wad of Wool",
+"Messy Scroll",
+];
+
+export const misc_secondary_list = ["Greenball Bun",
+"Tako Bug"
+];
+
+export const primaries = [
+    metal_list,
+    wood_list,
+    stone_list,
+    hide_list,
+    scales_list,
+    bone_list,
+    fabric_list,
+    rock_list,
+    misc_list
+];
+
+export const primaries_names =[
+    "metals",
+    "woods",
+    "stones",
+    "hides",
+    "scales",
+    "bones",
+    "fabrics",
+    "rocks",
+    "misc"
+];
+
+export const secondaries = [
+    coin_list,
+    mana_stone_list,
+    mana_crystal_list,
+    seed_list,
+    produce_list,
+    meat_list,
+    fang_list,
+    eye_list,
+    feather_list,
+    bottle_list,
+    vial_list,
+    urn_list,
+    pellet_list,
+    powder_list,
+    pouch_list,
+    misc_secondary_list
+];
+export const secondaries_names = [
+    "coins",
+    "mana stones",
+    "mana crystals",
+    "seeds",
+    "produces",
+    "meats",
+    "fangs and claws",
+    "eyes",
+    "feathers",
+    "bottles",
+    'vials',
+    "urns",
+    "pellets",
+    "powders",
+    "pouches",
+    "secondary misc"
+];
